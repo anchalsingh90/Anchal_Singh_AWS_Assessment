574df682b9737d19a4f1efaf34333a807f86a04e
Anchal Singh <149249683+anchalsingh90@users.noreply.github.com>
2025-12-04 20:04:04 +0530

    Update main.tf

--- 1_VPC_Networking/2_EC2_Static_Website/main.tf
+++ 1_VPC_Networking/2_EC2_Static_Website/main.tf
@@ -2,42 +2,57 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Target Group
-resource "aws_lb_target_group" "tg" {
-  name     = "anchal-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.anchal_vpc.id
-}
+# Security group for EC2
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_static_website_sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.anchal_vpc.id
 
-# Load Balancer
-resource "aws_lb" "alb" {
-  name               = "anchal-alb"
-  load_balancer_type = "application"
-  subnets            = [
-    aws_subnet.public_sub1.id,
-    aws_subnet.public_sub2.id
-  ]
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  security_groups = [aws_security_group.ec2_sg.id]
-}
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# Listener
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+  tags = {
+    Name = "EC2_Static_Website_SG"
   }
 }
 
-# Attach EC2 to Target Group
-resource "aws_lb_target_group_attachment" "attach_ec2" {
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_instance.static_web.id
-  port             = 80
-}
+# EC2 Instance
+resource "aws_instance" "static_web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_sub1.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install httpd -y
+              systemctl enable httpd
+              systemctl start httpd
+              echo "Welcome to Anchal's Static Website" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "Anchal_Static_Website_EC2"
+  }
+}
